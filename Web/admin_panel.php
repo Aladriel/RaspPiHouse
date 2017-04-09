b750c3f36e1daf411ecd820c3a1393685c2c5caa
Marcin Pancerz <marcin2523@o2.pl>
2017-04-09 10:57:55 +0200

    Done

--- Web/admin_panel.php
+++ Web/admin_panel.php
@@ -9,32 +9,130 @@ if(!isset($_SESSION['logged_in']))
 }
 ?>
 <!DOCTYPE html>
-<html lang = "pl">
-<head>
-
+<html lang="pl">
+  <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		
-    <title>Smart House</title>
-    <meta name = "description" content="">
-    <meta name = "keywords" content="">
-    <meta name = "author" content="">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico"><!-- ! -->
 
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <script src = "js/code.js"></script>
-</head>
-<body>
+    <title>Pi house settings</title>
+	
+	
+    <link href="css/mobile.css" rel="stylesheet">
+	
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/layout.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     
-    <?php
-        
-    echo "<p>WITAJ ".$_SESSION['user']."!";
-    echo "<a href=logout.php>Log out!</a>";
-	
-				
-	?>
-	
+        <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+    
+  </head>
 
-</body>
+  <body>
 
+  
+    <div class="container">
+
+      <!-- Static navbar -->
+     <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand menu_mobilne" href="admin_panel.php">Pi house</a>
+			   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+                <span class="sr-only">Rozwiń nawigację</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+            <ul class="nav navbar-nav">
+              <li class="active" role="presentation"><a href="admin_panel.php">Home</a></li>
+              <li role="presentation"><a href="#">About</a></li>
+              <li role="presentation"><a href="#">Contact</a></li>
+              <li role="presentation" class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Action<span class="caret"></span></a>
+                <ul class="dropdown-menu" >
+                  <li role="presentation" ><a href="addUser.php">Add user</a></li>
+                  <li role="presentation"><a href="privileges.php">Change user privileges</a></li>
+                  <li role="presentation"><a href="addSensor.php">Add sensor</a></li>
+                </ul>
+              </li>
+            </ul>
+			<div class = "nav navbar-nav" id = "logout">
+				<?php   
+					echo "<a href=logout.php class='btn btn-info btn-lg'>";
+					echo "<span class='glyphicon glyphicon-log-out'></span> Log out";
+					echo"</a>";
+				?>
+			</div>
+              
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+ 
+
+      <div class="page-header">
+        <h1>Welcome to the Administrator Panel!</h1>
+      </div>
+
+      
+      
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/ic_settings_input_antenna_black_48dp_2x.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Sensors</h2>
+          <p>Here you can add new sensors that will be handled by the Rasp Pi House.</p>
+          <p><a class="btn btn-default" href="addSensor.php" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/ic_person_outline_black_48dp_2x.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Users</h2>
+          <p>Here you can add new users to the system.</p>
+          <p><a class="btn btn-default" href="addUser.php" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="images/ic_supervisor_account_black_48dp_2x.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Privileges</h2>
+          <p>Go here, if you want to change privileges of a particular user.</p>
+          <p><a class="btn btn-default" href="privileges.php" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+    </div><!-- container marketing -->
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
