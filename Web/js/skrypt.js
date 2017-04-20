4fc0d3944c2163d11b646e9a749de2bc2dea8545
Marcin Pancerz <marcin2523@o2.pl>
2017-04-20 21:21:13 +0200

    Check in options

--- Web/js/skrypt.js
+++ Web/js/skrypt.js
@@ -40,7 +40,7 @@ function main()
 			if(tmp == '0')
 				wynik = document.createTextNode('1');
 			else
-				wynik = document.createTextNode('0');
+				wynik = document.createTextNode('0');	
 		}
 		
 		a.appendChild(wynik);
@@ -49,8 +49,50 @@ function main()
 }
 
 
-function jsOnClick()
-{
-	
-	
-}
+function onclickHandler(xyz)
+    {
+		  var separator ="_e_";
+		  var privilege = document.getElementById('privilege').value;
+		  var id = xyz.id;
+		  
+		  var a = id.split(separator);
+		  var tabela = document.getElementById('inputs');
+		
+		  
+		  tabela.deleteRow(a[1]);
+
+		  var tmp ="";
+		  var class_name="";
+		  var answer = document.getElementById('endPrivilege');
+		  
+		  if(a[2]=="0")
+		  {
+			tmp= a[0]+separator+a[1]+separator+"1";
+			class_name="fontello_icon_green";
+			
+			answer.value = parseInt(answer.value) + Math.pow(2, parseInt(a[1]));
+		  }
+		  
+		  if(a[2]=="1")
+		  {
+			tmp = a[0]+separator+a[1]+separator+"0";
+			class_name="fontello_icon";
+			answer.value = parseInt(answer.value) - Math.pow(2, parseInt(a[1]));
+		  }
+
+		  	var row = tabela.insertRow(a[1]);
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+		  	cell1.innerHTML = a[0].replace("_"," ");
+			cell2.innerHTML = '<i onclick="onclickHandler('+tmp+')"; class="icon-ok-circled '+class_name+'" id = '+tmp+'></i></td>';
+		  
+			
+			//alert(answer.value);
+			
+			
+			
+	       //  alert( a[0] + " " + a[1] + " " + a[2]  );
+		  
+      
+
+    }
